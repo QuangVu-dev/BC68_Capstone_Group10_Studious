20db28ef5793ad78262c5a878e8d93bec3a70e63
QuangVu-dev <tqvu97@gmail.com>
2024-04-26 10:50:47 +0700

    final code

---
+++ js/index.js
@@ -0,0 +1,28 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("site-header").style.height = "70px";
+  } else {
+    document.getElementById("site-header").style.height = "90px";
+  }
+}
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navigation").style.padding = "0.5rem 1rem";
+    document.getElementById("site-header").style.boxShadow =
+      "0px 3px 6px 0px rgba(0, 0, 0, 0.05";
+    document.getElementById("site-header").style.backgroundColor = "#fff";
+  } else {
+    document.getElementById("navigation").style.padding = "1.2rem 1rem";
+    document.getElementById("site-header").style.padding = "none";
+    document.getElementById("site-header").style.backgroundColor =
+      "transparent";
+  }
+}
+
+// Dark mode
